2300bb8aed80ecbc7d6be81a439b4be18ce4b44c
Italo Joel Sandoval Amoretti <79050552+ijoelsandoval@users.noreply.github.com>
2021-02-18 18:47:28 +0100

    Hola mundo v2

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,8 +1,8 @@
 print("Hello World!")
 print("Hola\nMundo!\nHello\nWorld!")
 print("El perro dijo: \"¡Guau!\"")
-print("""You can print
-on multiple
-lines using
-triple
-quotes.""")
+print("""Buenos dias
+Hoy
+fue
+un dia 
+perfecto""")
