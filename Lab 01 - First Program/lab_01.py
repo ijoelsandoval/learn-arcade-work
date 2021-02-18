9e50bca92956068eb5b47e53830caab5885c0287
Italo Joel Sandoval Amoretti <79050552+ijoelsandoval@users.noreply.github.com>
2021-02-18 18:38:51 +0100

    Hola mundo

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,8 @@
-hi
+print("Hello World!")
+print("Hola\nMundo!\nHello\nWorld!")
+print("El perro dijo: \"¡Guau!\"")
+print("""You can print
+on multiple
+lines using
+triple
+quotes.""")
---
+++ Testing/test.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("Hola\nMundo!\nHello\nWorld!")
+print("El perro dijo: \"¡Guau!\"")
+print("""You can print
+on multiple
+lines using
+triple
+quotes.""")
